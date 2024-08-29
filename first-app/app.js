17738ab781782af500c380f8bbc9025f63eeddba
farhan-alam18 <farhanalam1113@gmail.com>
2024-08-30 00:34:49 +0530

    fs Module

--- first-app/app.js
+++ first-app/app.js
@@ -6,3 +6,11 @@ console.log(os.freemem())
 console.log(os.totalmem())
 console.log(os.hostname())
 console.log(os.type())
+
+const fs = require('fs')
+// const files = fs.readdirSync('./')
+
+fs.readdir('$',  (err,files)=> {
+  if(err) console.log(err)
+    else console.log(files)
+} )

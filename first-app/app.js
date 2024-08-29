74efbf83844a1e510d541be99069794fe1e131d8
farhan-alam18 <farhanalam1113@gmail.com>
2024-08-29 21:45:13 +0530

    Initial Commit

---
+++ first-app/app.js
@@ -0,0 +1,6 @@
+const logger = require('./logger')
+
+// console.log(logger)
+logger('message')
+console.log(__filename)
+console.log(__dirname)
---
+++ first-app/logger.js
@@ -0,0 +1,8 @@
+var url = "http://mylogger.io/log"
+
+function Log(message){
+  // Sending a HTTP Request : 
+  console.log(message);
+}
+
+module.exports = Log;
